68787b414143ba8892b665285c096510de65ed62
TIMA <64804931+turatsamar@users.noreply.github.com>
2023-05-14 23:25:12 +0900

    Create IfandIf.c
    how work If condition

---
+++ IfandIf.c
@@ -0,0 +1,21 @@
+int main(){
+
+    int age;
+    printf("Toigo kelgeniniske rahmat!!\n");
+    printf("---------------------------\n");
+    printf("Jash kuragynysty korsotunuz ");
+    scanf("%d",&age);
+
+    if(age<6){
+        printf("sizge beker!!");
+    }
+
+    if(age>6)
+    {
+        printf("kiruu akysy 10000 som!!\n");
+    }
+    printf("-----------------------------\n");
+    printf("Jakshy konul achyp ketiniz!!\n");
+    
+    return 0;
+}
