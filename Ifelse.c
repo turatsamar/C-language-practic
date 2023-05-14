7e6539da16db00e917e54328e5f26d649597e6ef
TIMA <64804931+turatsamar@users.noreply.github.com>
2023-05-14 23:28:42 +0900

    Create Ifelse.c
    how work if else condition

---
+++ Ifelse.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int age;
+    printf("Toigo kelgeniniske rahmat!!\n");
+    printf("---------------------------\n");
+    printf("Jash kuragynysty korsotunuz ");
+    scanf("%d",&age);
+
+    if(age<6){
+        printf("sizge beker!!");
+    }
+
+    else{
+        printf("Kiruu akysy 1000 som\n");
+    }
+    printf("-----------------------------\n");
+    printf("Jakshy konul achyp ketiniz!!\n");
+    
+    return 0;
+}
